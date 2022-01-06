923cb0fdaaa52be16a5a2741089eca09dd5ec1c7
ruishan <ruishan@debian-BULLSEYE-live-builder-AMD64>
2022-01-06 16:57:45 -0600

    Migrated to using Whiptail as user interface

--- PackageInstallScript.sh
+++ PackageInstallScript.sh
@@ -3,23 +3,23 @@
 #Package install script
 #This will guide a user through installing packages through apt.
 
-dialog --msgbox "Welcome to Package Install!" 0 0
-clear
-dialog --msgbox "Updating your system..." 0 0
+whiptail --msgbox --title "Hello!" "Welcome to Package Install!" 0 0
+
+whiptail --msgbox "Updating your system..." 0 0
 clear
 sudo apt update
 echo "Type anything to continue."
 read
 clear
 
-packagename=$(dialog --title "Package Install" --inputbox "Enter the name of the package you want to install." 0 0 \
+packagename=$(whiptail --inputbox --title "Package Install" "Enter the name of the package you want to install." 0 0 \
 3>&1 1>&2 2>&3 3>&- \
 )
-clear
+
 
 sudo apt install $packagename
 clear
 echo "Type anything to continue."
 read
-dialog --msgbox "Done! Thank you for using this script!" 0 0
+whiptail --msgbox "Done! Thank you for using this script!" 0 0
 clear
